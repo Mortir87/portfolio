e658883b81d5bb0a586ba59f26653e76b8a4f0d1
Aitor <Aitor@LM.Negro>
2026-06-10 16:35:03 +0200

    Configurado Astro para despliegue en Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+
+export default defineConfig({
+  adapter: vercel(),
+});
+
 
